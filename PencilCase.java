74c37d4e0f7f72b238dd5f9ab1101e7206b93d6c
adilahdiyana <81138230+adilahdiyana@users.noreply.github.com>
2021-04-18 16:04:23 +0800

    Update PencilCase.java

--- PencilCase.java
+++ PencilCase.java
@@ -1,60 +1,58 @@
 package PencilCaseConstructor;
-
 import java.util.Scanner;
-
 public class PencilCase {
 	
-	String brand;
-	String color;
-	String material;
-	Boolean shape;
-	double dimensions;
-	double price;
-	double weight;
-	double rating;
-	
-	Scanner sc = new Scanner(System.in);
-	
-	// default constructor
-	PencilCase(){
+		String brand;
+		String color;
+		String material;
+		String shape;
+		double dimensions;
+		double price;
+		double weight;
+		double rating;
 		
-		System.out.println("Brand name \t: ");
-		this.brand = sc.nextLine();
+		Scanner sc = new Scanner(System.in);
 		
-		System.out.println("Color of brand \t: ");
-		this.color = sc.nextLine();
-		
-		System.out.println("Material of brand \t: ");
-		this.material = sc.nextLine();
-		
-		System.out.println("Shape of brand? (cylindrical/round): ");
-		this.shape = sc.nextBoolean();
-		
-		System.out.println("Dimensions of brand \t: ");
-		this.dimensions = sc.nextDouble();
-		
-		System.out.println("Price of brand \t: ");
-		this.price = sc.nextDouble();
-		
-		System.out.println("Weight of brand \t: ");
-		this.weight = sc.nextDouble();
-		
-		System.out.println("Rating of brand: \t");
-		this.rating = sc.nextDouble();
-	
-	}
-		
-		// Parameterized constructor
-		PencilCase(String brand, String color, String material, Boolean shape, double dimensions, double price, double weight, double rating){
-			this.brand = brand;
-			this.color = color;
-			this.material = material;
-			this.shape = shape;
-			this.dimensions = dimensions;
-			this.price = price;
-			this.weight = weight;
-			this.rating = rating;
+		// default constructor
+		PencilCase(){
 			
-		}	
+			System.out.println("Brand name : ");
+			this.brand = sc.nextLine();
+			
+			System.out.println("Color of brand : ");
+			this.color = sc.nextLine();
+			
+			System.out.println("Material of brand : ");
+			this.material = sc.nextLine();
+			
+			System.out.println("Shape of case? : ");
+			this.shape = sc.next();
+			
+			System.out.println("Dimensions of brand : ");
+			this.dimensions = sc.nextDouble();
+			
+			System.out.println("Price of brand : ");
+			this.price = sc.nextDouble();
+			
+			System.out.println("Weight of brand : ");
+			this.weight = sc.nextDouble();
+			
+			System.out.println("Rating of brand : ");
+			this.rating = sc.nextDouble();
+		
+		}
+			
+			// Parameterized constructor
+		PencilCase(String brand, String color, String material, String shape, double dimensions, double price, double weight, double rating){
+				this.brand = brand;
+				this.color = color;
+				this.material = material;
+				this.shape = shape;
+				this.dimensions = dimensions;
+				this.price = price;
+				this.weight = weight;
+				this.rating = rating;
+				
+			}	
 
-}
+	}
